292111b31dfa5ad44de21735c6e55e4277540539
ZY <746234304@qq.com>
2018-08-20 23:23:59 +0800

    初始

---
+++ src/main/java/com/example/demo/UnnameApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnnameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnnameApplication.class, args);
+	}
+}
