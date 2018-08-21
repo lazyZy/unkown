2f3d8e552a6523ff261e6762bb94740ee70704af
ZY <746234304@qq.com>
2018-08-21 14:52:55 +0800

    钉钉测试

--- src/main/java/com/example/demo/UnnameApplication.java
+++ src/main/java/com/example/demo/UnnameApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UnnameApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(UnnameApplication.class, args);
 	}
 }
